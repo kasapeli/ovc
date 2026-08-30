f9dd94cf29460d3f3282bc522a8ddbe52a2868db
kasapeli <yanratanaksambath@gmail.com>
2026-08-30 23:47:37 +0700

    args.rs: remove short argument for clean and add help info

--- src/modules/args.rs
+++ src/modules/args.rs
@@ -3,18 +3,23 @@ use std::path::PathBuf;
 
 #[derive(Parser)]
 pub struct Args {
+    /// Initialize repository
     #[arg(short = 'i', long = "init")]
     pub init_repo: bool,
 
-    #[arg(short = 'C', long = "CLEAN")]
+    // Uninitialize repository
+    #[arg(long = "CLEAN")]
     pub clean_repo: bool,
 
+    // Stage file
     #[arg(short = 's', long = "stage", value_name = "FILE")]
     pub stage_file: Option<PathBuf>,
 
+    // Check status
     #[arg(short = 'S', long = "status")]
     pub repo_status: bool,
 
+    // Commit
     #[arg(short = 'c', long = "commit", value_name = "MESSAGE")]
     pub message: Option<String>,
 }
